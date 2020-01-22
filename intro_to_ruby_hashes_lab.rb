c7524b4299106db946774bbd249a0258bc85cf78
Joseph S. Cha <josephshincha@gmail.com>
2020-01-22 03:19:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  new_hast = {
+  new_hash = {
     key => value
   }
 end
