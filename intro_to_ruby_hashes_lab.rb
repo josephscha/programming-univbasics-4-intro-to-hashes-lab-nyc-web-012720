6fe50ecd65e11ace913a320fb9195b37cf1faff8
Joseph S. Cha <josephshincha@gmail.com>
2020-01-22 03:14:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,14 @@
 def new_hash
   # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  grocery_list = {
+    "Eggs" => "Dozen",
+    "Apples" => "Half dozen"
+  }
 end
 
 def pioneer
